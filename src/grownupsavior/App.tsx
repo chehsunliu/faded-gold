07c0ab14600767fd572092eab4908cafd16969dc
chehsunliu <chehsunliu@gmail.com>
2021-10-24 19:11:10 +0800

    Add prettier

--- src/grownupsavior/App.tsx
+++ src/grownupsavior/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Home</h2>
+        <p>This is a sentence.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>About</h2>
+        <p>This is another sentence.</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-      123
+      <h1>Welcome!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
