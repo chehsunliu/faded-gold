3d809e95ea6b3d1fda515d10855b51c19c7af067
chehsunliu <chehsunliu@gmail.com>
2021-10-24 19:49:37 +0800

    Simplify

--- src/grownupsavior/App.test.tsx
+++ src/grownupsavior/App.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "grownupsavior/App";
 
-test("renders learn react link", () => {
+test("renders 123", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/123/i);
   expect(linkElement).toBeInTheDocument();
 });
