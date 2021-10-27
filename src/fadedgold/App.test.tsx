29e48e2139a3f6ec49185b4a6a3525ddfb71885f
chehsunliu <chehsunliu@gmail.com>
2021-10-28 00:30:20 +0800

    Add menu

--- src/fadedgold/App.test.tsx
+++ src/fadedgold/App.test.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { render, screen } from "fadedgold/test-utils";
+import { render } from "fadedgold/test-utils";
 import App from "fadedgold/App";
 
 test("renders 123", () => {
   render(<App />);
-  const linkElement = screen.getByText(/123/i);
-  expect(linkElement).toBeInTheDocument();
 });
---
+++ src/fadedgold/routingConfig.tsx
@@ -0,0 +1,29 @@
+import { SWD3E } from "fadedgold/pages/swd3e";
+import React from "react";
+import { Home } from "fadedgold/pages/home";
+
+export interface RoutingPageProps {
+  title: string;
+}
+
+export type RoutingItems = { [key: string]: RoutingItem };
+
+interface RoutingItem {
+  url: string;
+  pageType: React.ComponentType<RoutingPageProps>;
+}
+
+export const routingItems: { [key: string]: RoutingItem } = {
+  home: {
+    url: "/",
+    pageType: Home,
+  },
+  swd2e: {
+    url: "/games/swd2e",
+    pageType: SWD3E,
+  },
+  swd3e: {
+    url: "/games/swd3e",
+    pageType: SWD3E,
+  },
+};
