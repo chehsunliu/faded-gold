3b03fe2c2068139939caf6c449aa306e04e42de5
chehsunliu <chehsunliu@gmail.com>
2021-10-30 01:07:58 +0800

    Add character state interface

---
+++ src/fadedgold/pages/swd3e/characterSlice.ts
@@ -0,0 +1,22 @@
+export interface CharacterState {
+  experience: number;
+
+  health: number;
+  mana: number;
+  vitality: number;
+  maxHealth: number;
+  maxMana: number;
+  maxVitality: number;
+
+  strength: number;
+  durability: number;
+  intelligence: number;
+  speed: number;
+
+  level: number;
+  metalResistance: number;
+  woodResistance: number;
+  waterResistance: number;
+  fireResistance: number;
+  earthResistance: number;
+}
