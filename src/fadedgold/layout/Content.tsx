601741744dfeea6de16a00c777e9f68b6a09254a
Che-Hsun Liu <chehsunliu@gmail.com>
2021-10-29 22:08:14 +0800

    Move title rendering outside (#13)

--- src/fadedgold/layout/Content.tsx
+++ src/fadedgold/layout/Content.tsx
@@ -1,6 +1,7 @@
 import { RoutingItems } from "fadedgold/routingConfig";
 import { Route, Routes } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Heading1 } from "fadedgold/component/text";
 
 interface ContentProps {
   routingItems: RoutingItems;
@@ -11,7 +12,12 @@ export const Content = (props: ContentProps) => {
   const { t } = useTranslation("menu");
 
   const routes = Object.entries(routingItems).map(([key, item]) => {
-    const element = <item.pageType title={t(key)} />;
+    const element = (
+      <>
+        <Heading1>{t(key)}</Heading1>
+        {item.element()}
+      </>
+    );
     return <Route key={key} path={item.url} element={element} />;
   });
 
--- src/fadedgold/routingConfig.tsx
+++ src/fadedgold/routingConfig.tsx
@@ -1,30 +1,26 @@
 import { SWD2E } from "fadedgold/pages/swd2e";
 import { SWD3E } from "fadedgold/pages/swd3e";
-import React from "react";
 import { Home } from "fadedgold/pages/Home";
-
-export interface RoutingPageProps {
-  title: string;
-}
+import { ReactElement } from "react";
 
 export type RoutingItems = { [key: string]: RoutingItem };
 
 interface RoutingItem {
   url: string;
-  pageType: React.ComponentType<RoutingPageProps>;
+  element: () => ReactElement;
 }
 
 export const routingItems: { [key: string]: RoutingItem } = {
   home: {
     url: "/",
-    pageType: Home,
+    element: () => <Home />,
   },
   swd2e: {
     url: "/games/swd2e",
-    pageType: SWD2E,
+    element: () => <SWD2E />,
   },
   swd3e: {
     url: "/games/swd3e",
-    pageType: SWD3E,
+    element: () => <SWD3E />,
   },
 };
