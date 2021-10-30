29cea0cd2e2e67eeef8ee90bedeb0466f7cc3445
chehsunliu <chehsunliu@gmail.com>
2021-10-31 02:04:00 +0800

    tmp

--- src/fadedgold/layout/Content.tsx
+++ src/fadedgold/layout/Content.tsx
@@ -2,10 +2,21 @@ import { RoutingItems } from "fadedgold/routingConfig";
 import { Route, Routes } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Heading1 } from "fadedgold/component/text";
+import { IStackStyles, IStackTokens, Stack } from "@fluentui/react";
 
 interface ContentProps {
   routingItems: RoutingItems;
 }
+
+const stackTokens: IStackTokens = {
+  childrenGap: 10,
+};
+
+const stackStyles: IStackStyles = {
+  root: {
+    display: "block",
+  },
+};
 
 export const Content = (props: ContentProps) => {
   const { routingItems } = props;
@@ -13,10 +24,10 @@ export const Content = (props: ContentProps) => {
 
   const routes = Object.entries(routingItems).map(([key, item]) => {
     const element = (
-      <>
+      <Stack tokens={stackTokens} styles={stackStyles}>
         <Heading1>{t(key)}</Heading1>
         {item.element()}
-      </>
+      </Stack>
     );
     return <Route key={key} path={item.url} element={element} />;
   });
