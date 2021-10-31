1b53ec81606d37f0f4a425fcb1e979ae9a2a5294
chehsunliu <chehsunliu@gmail.com>
2021-11-01 04:28:06 +0800

    Reformat

--- src/fadedgold/pages/Home.tsx
+++ src/fadedgold/pages/Home.tsx
@@ -3,9 +3,7 @@ import { Paragraph } from "fadedgold/component/text";
 export const Home = () => {
   return (
     <>
-      <Paragraph>
-        TBD
-      </Paragraph>
+      <Paragraph>TBD</Paragraph>
     </>
   );
 };
