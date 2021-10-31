3af8582d03e35a4968151d50e58cb09573865821
chehsunliu <chehsunliu@gmail.com>
2021-11-01 02:55:34 +0800

    Reformat

--- src/fadedgold/pages/swd3e/editor.ts
+++ src/fadedgold/pages/swd3e/editor.ts
@@ -1,6 +1,27 @@
 import { Character, PartyState } from "fadedgold/pages/swd3e/partySlice";
 import { GameState } from "fadedgold/pages/swd3e/gameSlice";
 
+const EXPERIENCE_ADDRESS = 0x129fe;
+
+const HEALTH_ADDRESS = EXPERIENCE_ADDRESS + 4;
+const MANA_ADDRESS = HEALTH_ADDRESS + 2;
+const VITALITY_ADDRESS = MANA_ADDRESS + 2;
+const MAX_HEALTH_ADDRESS = VITALITY_ADDRESS + 2;
+const MAX_MANA_ADDRESS = MAX_HEALTH_ADDRESS + 2;
+const MAX_VITALITY_ADDRESS = MAX_MANA_ADDRESS + 2;
+
+const STRENGTH_ADDRESS = MAX_VITALITY_ADDRESS + 2;
+const DURABILITY_ADDRESS = STRENGTH_ADDRESS + 2;
+const INTELLIGENCE_ADDRESS = DURABILITY_ADDRESS + 2;
+const SPEED_ADDRESS = INTELLIGENCE_ADDRESS + 2;
+
+const LEVEL_ADDRESS = 0x12a2a;
+const METAL_RESISTANCE_ADDRESS = LEVEL_ADDRESS + 1;
+const WOOD_RESISTANCE_ADDRESS = METAL_RESISTANCE_ADDRESS + 1;
+const WATER_RESISTANCE_ADDRESS = WOOD_RESISTANCE_ADDRESS + 1;
+const FIRE_RESISTANCE_ADDRESS = WATER_RESISTANCE_ADDRESS + 1;
+const EARTH_RESISTANCE_ADDRESS = FIRE_RESISTANCE_ADDRESS + 1;
+
 interface GameInfo {
   game: GameState;
   party: PartyState;
@@ -15,48 +36,27 @@ const loadGame = (data: DataView): GameState => {
 };
 
 const loadCharacter = (data: DataView, offset: number): Character => {
-  const EXPERIENCE_ADDRESS = 0x129fe + offset;
-
-  const HEALTH_ADDRESS = EXPERIENCE_ADDRESS + 4;
-  const MANA_ADDRESS = HEALTH_ADDRESS + 2;
-  const VITALITY_ADDRESS = MANA_ADDRESS + 2;
-  const MAX_HEALTH_ADDRESS = VITALITY_ADDRESS + 2;
-  const MAX_MANA_ADDRESS = MAX_HEALTH_ADDRESS + 2;
-  const MAX_VITALITY_ADDRESS = MAX_MANA_ADDRESS + 2;
-
-  const STRENGTH_ADDRESS = MAX_VITALITY_ADDRESS + 2;
-  const DURABILITY_ADDRESS = STRENGTH_ADDRESS + 2;
-  const INTELLIGENCE_ADDRESS = DURABILITY_ADDRESS + 2;
-  const SPEED_ADDRESS = INTELLIGENCE_ADDRESS + 2;
-
-  const LEVEL_ADDRESS = 0x12a2a + offset;
-  const METAL_RESISTANCE_ADDRESS = LEVEL_ADDRESS + 1;
-  const WOOD_RESISTANCE_ADDRESS = METAL_RESISTANCE_ADDRESS + 1;
-  const WATER_RESISTANCE_ADDRESS = WOOD_RESISTANCE_ADDRESS + 1;
-  const FIRE_RESISTANCE_ADDRESS = WATER_RESISTANCE_ADDRESS + 1;
-  const EARTH_RESISTANCE_ADDRESS = FIRE_RESISTANCE_ADDRESS + 1;
-
   return {
-    experience: data.getUint32(EXPERIENCE_ADDRESS, true),
+    experience: data.getUint32(EXPERIENCE_ADDRESS + offset, true),
 
-    health: data.getUint16(HEALTH_ADDRESS, true),
-    mana: data.getUint16(MANA_ADDRESS, true),
-    vitality: data.getUint16(VITALITY_ADDRESS, true),
-    maxHealth: data.getUint16(MAX_HEALTH_ADDRESS, true),
-    maxMana: data.getUint16(MAX_MANA_ADDRESS, true),
-    maxVitality: data.getUint16(MAX_VITALITY_ADDRESS, true),
+    health: data.getUint16(HEALTH_ADDRESS + offset, true),
+    mana: data.getUint16(MANA_ADDRESS + offset, true),
+    vitality: data.getUint16(VITALITY_ADDRESS + offset, true),
+    maxHealth: data.getUint16(MAX_HEALTH_ADDRESS + offset, true),
+    maxMana: data.getUint16(MAX_MANA_ADDRESS + offset, true),
+    maxVitality: data.getUint16(MAX_VITALITY_ADDRESS + offset, true),
 
-    strength: data.getUint16(STRENGTH_ADDRESS, true),
-    durability: data.getUint16(DURABILITY_ADDRESS, true),
-    intelligence: data.getUint16(INTELLIGENCE_ADDRESS, true),
-    speed: data.getUint16(SPEED_ADDRESS, true),
+    strength: data.getUint16(STRENGTH_ADDRESS + offset, true),
+    durability: data.getUint16(DURABILITY_ADDRESS + offset, true),
+    intelligence: data.getUint16(INTELLIGENCE_ADDRESS + offset, true),
+    speed: data.getUint16(SPEED_ADDRESS + offset, true),
 
-    level: data.getUint8(LEVEL_ADDRESS),
-    metalResistance: data.getUint8(METAL_RESISTANCE_ADDRESS),
-    woodResistance: data.getUint8(WOOD_RESISTANCE_ADDRESS),
-    waterResistance: data.getUint8(WATER_RESISTANCE_ADDRESS),
-    fireResistance: data.getUint8(FIRE_RESISTANCE_ADDRESS),
-    earthResistance: data.getUint8(EARTH_RESISTANCE_ADDRESS),
+    level: data.getUint8(LEVEL_ADDRESS + offset),
+    metalResistance: data.getUint8(METAL_RESISTANCE_ADDRESS + offset),
+    woodResistance: data.getUint8(WOOD_RESISTANCE_ADDRESS + offset),
+    waterResistance: data.getUint8(WATER_RESISTANCE_ADDRESS + offset),
+    fireResistance: data.getUint8(FIRE_RESISTANCE_ADDRESS + offset),
+    earthResistance: data.getUint8(EARTH_RESISTANCE_ADDRESS + offset),
   };
 };
 
