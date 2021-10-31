fb327f43bb7f6c2b1207a630b3c28062595d1324
chehsunliu <chehsunliu@gmail.com>
2021-10-31 18:57:31 +0800

    Add numeric text field

--- src/fadedgold/pages/swd3e/GameProgress.tsx
+++ src/fadedgold/pages/swd3e/GameProgress.tsx
@@ -2,50 +2,27 @@ import { Heading2 } from "fadedgold/component/text";
 import { useTranslation } from "react-i18next";
 import { useAppDispatch, useAppSelector } from "fadedgold/redux/hook";
 import { actions } from "fadedgold/pages/swd3e/gameSlice";
-import { ITextFieldStyles, TextField } from "@fluentui/react";
-import { FormEvent } from "react";
-
-const textFieldsStyles: Partial<ITextFieldStyles> = {
-  fieldGroup: {
-    width: 100,
-  },
-};
+import { NumericTextField } from "fadedgold/component/NumericTextField";
 
 export const GameProgress = () => {
   const { t } = useTranslation("swd3e", { keyPrefix: "game" });
   const dispatch = useAppDispatch();
   const { money } = useAppSelector((state) => state.swd3e.game);
 
-  const handleMoneyChange = (
-    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>,
-    raw?: string
-  ) => {
-    if (raw === undefined) {
-      return;
-    }
-
-    if (raw.length === 0) {
-      dispatch(actions.moneyUpdated(0));
-      return;
-    }
-
-    const value = parseInt(raw);
-    if (isNaN(value)) {
-      return;
-    }
-
+  const handleMoneyChange = (value: number) => {
     dispatch(actions.moneyUpdated(value));
   };
 
   return (
-    <>
+    <div>
       <Heading2>{t("title")}</Heading2>
-      <TextField
+      <NumericTextField
         label={t("money")}
-        value={`${money}`}
+        value={money}
+        maxValue={0xffffffff}
+        width={100}
         onChange={handleMoneyChange}
-        styles={textFieldsStyles}
       />
-    </>
+    </div>
   );
 };
